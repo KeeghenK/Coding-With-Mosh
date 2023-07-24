dcd180912fdf27d43208c46786eaccf577352d73
Keeghen Kessler <kjrkessler@gmail.com>
2023-07-24 12:53:50 -0500

    Redux practice- fundamentals

--- Redux/redux-starter/src/index.js
+++ Redux/redux-starter/src/index.js
@@ -1,3 +1,7 @@
+import { bugAdded, bugResolved } from "./actions";
 import store from "./store";
 
-console.log(store);
+store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugResolved(1));
+
+console.log(store.getState());
--- Redux/redux-starter/src/reducer.js
+++ Redux/redux-starter/src/reducer.js
@@ -1,8 +1,10 @@
+import * as actions from "./actionTypes";
+
 let lastId = 0;
 
 export default function reducer(state = [], action) {
 	switch (action.type) {
-		case "bugAdded":
+		case actions.BUG_ADDED:
 			return [
 				...state,
 				{
@@ -11,8 +13,12 @@ export default function reducer(state = [], action) {
 					resolve: false,
 				},
 			];
-		case "bugRemoved":
+		case actions.BUG_REMOVED:
 			return state.filter((bug) => bug.id !== action.payload.id);
+		case actions.BUG_RESOLVED:
+			return state.map((bug) =>
+				bug.id === action.payload.id ? { ...bug, resolve: true } : bug
+			);
 		default:
 			return state;
 	}
