24951f100fefbc71215714027bb6d87e0e3230c5
Keeghen Kessler <kjrkessler@gmail.com>
2023-07-25 10:32:58 -0500

    Redux practice- practice tests

--- Redux/redux-starter/src/math.spec.js
+++ Redux/redux-starter/src/math.spec.js
@@ -1,1 +1,13 @@
-it("first test", () => {});
+import { isEven } from "./math";
+
+describe("isEven", () => {
+	it("should return true if number is even", () => {
+		const result = isEven(2);
+		expect(result).toEqual(true);
+	});
+
+	it("should return false if number is odd", () => {
+		const result = isEven(1);
+		expect(result).toEqual(false);
+	});
+});
